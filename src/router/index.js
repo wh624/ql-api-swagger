c7f5299838eac270c99c8834c8bd0f9c34a5a702
promisewu <promisewu@tencent.com>
2026-07-18 19:48:14 +0800

    feat: 单页面Tabs布局+修复Apifox导入URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ApiModuleView from '../views/ApiModuleView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/api/:moduleId', name: 'api-module', component: ApiModuleView, props: true }
+    { path: '/', name: 'home', component: HomeView }
   ]
 })
 
